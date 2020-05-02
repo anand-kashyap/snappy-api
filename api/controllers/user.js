dcb15513bd3a80e83bcf1e12b9735a27b6ea1c7b
Anand Kashyap <anandkashyap60@gmail.com>
2020-05-02 17:51:07 +0530

    added create user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -7,10 +7,15 @@ const userList = (req, res, next) => {
 const registerUser = (req, res) => {
 
   const { body: userObj } = req;
-  console.log(userObj);
+  // console.log(userObj);
+  const newUser = new User(userObj);
+  return newUser.save().then(
+    () => res.status(201).json({ success: 'user registered!' })
+  ).catch(e => {
+    console.log(e.errmsg);
+    res.status(400).json({ message: 'Error creating User' })
+  });
 
-  // const newUser = new User(userObj);
-  res.status(201).json({ success: 'user registered!' });
 }
 
 module.exports = { userList, registerUser };
